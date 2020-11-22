1ccae14f07a6087782f08258978be58ef95639d0
Pakvothe <franqitto@gmail.com>
2020-11-22 16:03:38 -0300

    implemented styled components

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStage } from '../gameHelpers.jsx';
+import { StyledTetris, StyledTetrisContainer } from './styles/StyledTetris.jsx';
 //Componentes
 import Stage from './Stage.jsx';
 import Display from './Display.jsx';
@@ -7,17 +8,19 @@ import StartButton from './StartButton.jsx';
 
 const Tetris = () => {
  return (
-  <div>
-   <Stage stage={createStage()} />
-   <aside>
-    <div>
-     <Display text="Score" />
-     <Display text="Rows" />
-     <Display text="Level" />
-    </div>
-    <StartButton />
-   </aside>
-  </div>
+  <StyledTetrisContainer>
+   <StyledTetris>
+    <Stage stage={createStage()} />
+    <aside>
+     <div>
+      <Display text="Score" />
+      <Display text="Rows" />
+      <Display text="Level" />
+     </div>
+     <StartButton />
+    </aside>
+   </StyledTetris>
+  </StyledTetrisContainer>
  );
 };
 
