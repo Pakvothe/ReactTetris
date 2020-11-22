fd7b37a67c698a086d48490e9e9cd68ba31e0f14
Pakvothe <franqitto@gmail.com>
2020-11-22 16:31:28 -0300

    implemented player, stage, speed and gameover States

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import { createStage } from '../gameHelpers.jsx';
+import React, { useState } from 'react';
+
+//Styled Componentes
 import { StyledTetris, StyledTetrisContainer } from './styles/StyledTetris.jsx';
-//Componentes
+
+//Custom Hooks
+import { usePlayer } from '../hooks/usePlayer.jsx';
+import { useStage } from '../hooks/useStage.jsx';
+
+//Components
 import Stage from './Stage.jsx';
 import Display from './Display.jsx';
 import StartButton from './StartButton.jsx';
 
 const Tetris = () => {
+ const [dropTime, setDropTime] = useState(null); // este estado maneja la velocidad dependiendo el nivel
+ const [gameOver, setGameOver] = useState(false);
+
+ const [player] = usePlayer();
+ const [stage, setStage] = useStage(player);
+
+ console.log('re-render');
  return (
   <StyledTetrisContainer>
    <StyledTetris>
-    <Stage stage={createStage()} />
+    <Stage stage={stage} />
     <aside>
-     <div>
-      <Display text="Score" />
-      <Display text="Rows" />
-      <Display text="Level" />
-     </div>
+     {gameOver ? (
+      <Display gameover={gameOver} text="Game Over" />
+     ) : (
+      <div>
+       <Display text="Score" />
+       <Display text="Rows" />
+       <Display text="Level" />
+      </div>
+     )}
      <StartButton />
     </aside>
    </StyledTetris>
