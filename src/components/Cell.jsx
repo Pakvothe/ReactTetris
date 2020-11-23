98e25dc6ef8e32da25bbff1837eed42a993a478a
Pakvothe <franqitto@gmail.com>
2020-11-23 01:18:34 -0300

    tetrominos collision implemented

--- src/components/Cell.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { StyledCell } from './styles/StyledCell.jsx';
-import { TETROMINOS } from '../tetrominos.jsx';
-
-const Cell = ({ type }) => (
- <StyledCell type={type} color={TETROMINOS[type].color} />
-);
-
-export default Cell;
--- src/components/StartButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyledStartButton } from './styles/StyledStartButton.jsx';
-
-const StartButton = ({ callback }) => (
- <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
-);
-
-export default StartButton;
