2822782d520a959b2a723f28616c2aedf0a14334
Pakvothe <franqitto@gmail.com>
2021-02-13 16:03:57 -0300

    fixed stage heigth in most monitor resolutions

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -13,7 +13,12 @@ import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 import { useGameStatus } from '../hooks/useGameStatus';
-import { setDropTime, setGameOver, setCurrentPlayer, setButtonCount } from '../redux/actions ';
+import {
+	setDropTime,
+	setGameOver,
+	setCurrentPlayer,
+	setButtonCount,
+} from '../redux/actions ';
 
 // Components
 import Stage from './Stage';
@@ -23,64 +28,66 @@ import Loading from './loading';
 
 const Tetris = () => {
 	const dispatch = useDispatch();
-	const dropTime = useSelector(state => state.dropTime);
-	const gameOver = useSelector(state => state.gameOver);
-	const currentPlayer = useSelector(state => state.currentPlayer);
+	const dropTime = useSelector((state) => state.dropTime);
+	const gameOver = useSelector((state) => state.gameOver);
+	const currentPlayer = useSelector((state) => state.currentPlayer);
 
 	const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
 	const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
-	const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
+	const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(
+		rowsCleared
+	);
 	const [loading, setLoading] = useState(true);
 	const [visit, setVisit] = useState(0);
 	const [puntaje, setPuntaje] = useState([
 		{
 			user: '',
-			number: 0
+			number: 0,
 		},
 		{
 			user: '',
-			number: 0
+			number: 0,
 		},
 		{
 			user: '',
-			number: 0
-		}
+			number: 0,
+		},
 	]);
 	//Firebase ==>
 
 	useEffect(() => {
 		const db = getFirestore();
-		const scores = db.collection('Tetris666').doc('FrancoTetris')
-		scores.get().then(puntajes => {
-			setLoading(false)
+		const scores = db.collection('Tetris666').doc('FrancoTetris');
+		scores.get().then((puntajes) => {
+			setLoading(false);
 			scores.update({
-				visits: firebase.firestore.FieldValue.increment(1)
-			})
+				visits: firebase.firestore.FieldValue.increment(1),
+			});
 			const data = puntajes.data();
-			setVisit(data.visits)
+			setVisit(data.visits);
 			setPuntaje([
 				{
 					user: data.maxName,
-					number: data.maxScore
+					number: data.maxScore,
 				},
 				{
 					user: data.secName,
-					number: data.secScore
+					number: data.secScore,
 				},
 				{
 					user: data.thirdName,
-					number: data.thirdScore
-				}
-			])
-		})
-	}, [])
+					number: data.thirdScore,
+				},
+			]);
+		});
+	}, []);
 
 	useEffect(() => {
 		if (gameOver) {
 			let objeto = {
 				user: currentPlayer,
-				number: score
-			}
+				number: score,
+			};
 			let array = [...puntaje];
 
 			if (score > puntaje[0].number) {
@@ -100,32 +107,34 @@ const Tetris = () => {
 			}
 
 			const db = getFirestore();
-			const scores = db.collection('Tetris666').doc('FrancoTetris')
-			scores.update({
-				maxName: array[0].user,
-				maxScore: array[0].number,
-				secName: array[1].user,
-				secScore: array[1].number,
-				thirdName: array[2].user,
-				thirdScore: array[2].number,
-			}).then(() => (
-				dispatch(setButtonCount(true))
-			)).then(
-				Swal.fire({
-					heightAuto: false,
-					title: 'New Score',
-					text: 'Congratz you beat a new score!',
-					icon: 'success',
-					confirmButtonText: 'Ok',
-					confirmButtonColor: '#ec85e1',
+			const scores = db.collection('Tetris666').doc('FrancoTetris');
+			scores
+				.update({
+					maxName: array[0].user,
+					maxScore: array[0].number,
+					secName: array[1].user,
+					secScore: array[1].number,
+					thirdName: array[2].user,
+					thirdScore: array[2].number,
 				})
-			).then()
+				.then(() => dispatch(setButtonCount(true)))
+				.then(
+					Swal.fire({
+						heightAuto: false,
+						title: 'New Score',
+						text: 'Congratz you beat a new score!',
+						icon: 'success',
+						confirmButtonText: 'Ok',
+						confirmButtonColor: '#ec85e1',
+					})
+				)
+				.then();
 		}
-	}, [gameOver, dispatch, currentPlayer, score])
+	}, [gameOver, dispatch, currentPlayer, score]);
 
 	//-------- <==
 
-	const movePlayer = dir => {
+	const movePlayer = (dir) => {
 		if (!checkCollision(player, stage, { x: dir, y: 0 })) {
 			updatePlayerPos({ x: dir, y: 0 });
 		}
@@ -153,7 +162,7 @@ const Tetris = () => {
 	const drop = () => {
 		// Increase level when player has cleared 10 rows
 		if (rows > (level + 1) * 10) {
-			setLevel(prev => prev + 1);
+			setLevel((prev) => prev + 1);
 			// Also increase speed
 			dispatch(setDropTime(1000 / (level + 1) + 200));
 		}
@@ -175,7 +184,6 @@ const Tetris = () => {
 		drop();
 	};
 
-
 	useInterval(() => {
 		drop();
 	}, dropTime);
@@ -196,12 +204,12 @@ const Tetris = () => {
 	const [input, setInput] = useState('');
 	const handleChange = (e) => {
 		setInput(e.target.value);
-	}
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		dispatch(setCurrentPlayer(input));
-	}
+	};
 
 	const handleError = (e) => {
 		e.preventDefault();
@@ -212,8 +220,8 @@ const Tetris = () => {
 			icon: 'error',
 			confirmButtonText: 'Ok',
 			confirmButtonColor: '#ec85e1',
-		})
-	}
+		});
+	};
 
 	return (
 		<>
@@ -221,57 +229,97 @@ const Tetris = () => {
 				<StyledTetrisWrapper
 					role="button"
 					tabIndex="0"
-					onKeyDown={e => move(e)}
+					onKeyDown={(e) => move(e)}
 					onKeyUp={keyUp}
 				>
 					<StyledTetris>
 						<Zoom left>
-							<div className='box_containers'>
-								<div className='info_container'>
-									<aside className='box_info'>
+							<div className="box_containers">
+								<div className="info_container">
+									<aside className="box_info">
 										<h2>Tetris Game</h2>
 										<div>
 											<p>Developed by</p>
 											<p>Franco Ortiz</p>
 										</div>
-										<a href='https://github.com/Pakvothe/ReactTetris' target='_blank' rel='noreferrer' className='amarillo'> ~ Repository ~ </a>
+										<a
+											href="https://github.com/Pakvothe/ReactTetris"
+											target="_blank"
+											rel="noreferrer"
+											className="amarillo"
+										>
+											{' '}
+											~ Repository ~{' '}
+										</a>
 									</aside>
-									<aside className='box_info'>
+									<aside className="box_info">
 										<h2>Controls</h2>
 										<div>
-											<p className='controls'><span className='amarillo'>⇧ </span>,  Spin Tetromino</p>
-											<p className='controls'><span className='amarillo'>⇩ </span>,  Fast Fall</p>
-											<p className='controls'><span className='amarillo'>⇨ </span>,  Right</p>
-											<p className='controls'><span className='amarillo'>⇦ </span>,  Left</p>
+											<p className="controls">
+												<span className="amarillo">
+													⇧{' '}
+												</span>
+												, Spin Tetromino
+											</p>
+											<p className="controls">
+												<span className="amarillo">
+													⇩{' '}
+												</span>
+												, Fast Fall
+											</p>
+											<p className="controls">
+												<span className="amarillo">
+													⇨{' '}
+												</span>
+												, Right
+											</p>
+											<p className="controls">
+												<span className="amarillo">
+													⇦{' '}
+												</span>
+												, Left
+											</p>
 										</div>
 									</aside>
 								</div>
-								<div className='info_container score_container'>
-									<aside className='box_info '>
-										<h2 >Scores</h2>
-										<div >
+								<div className="info_container score_container">
+									<aside className="box_info ">
+										<h2>Scores</h2>
+										<div>
 											<ul>
 												<li>
 													<h4>Max Score</h4>
-													<p className='amarillo'>{puntaje[0].user}</p>
-													<p className='amarillo'>{puntaje[0].number}</p>
+													<p className="amarillo">
+														{puntaje[0].user}
+													</p>
+													<p className="amarillo">
+														{puntaje[0].number}
+													</p>
 												</li>
 												<li>
 													<h4>Second Score</h4>
-													<p className='amarillo'>{puntaje[1].user}</p>
-													<p className='amarillo'>{puntaje[1].number}</p>
+													<p className="amarillo">
+														{puntaje[1].user}
+													</p>
+													<p className="amarillo">
+														{puntaje[1].number}
+													</p>
 												</li>
 												<li>
 													<h4>Third Score</h4>
-													<p className='amarillo'>{puntaje[2].user}</p>
-													<p className='amarillo'>{puntaje[2].number}</p>
+													<p className="amarillo">
+														{puntaje[2].user}
+													</p>
+													<p className="amarillo">
+														{puntaje[2].number}
+													</p>
 												</li>
 											</ul>
 										</div>
 									</aside>
-									<aside className='box_info '>
+									<aside className="box_info ">
 										<h4>Visits</h4>
-										<p className='amarillo'>{visit}</p>
+										<p className="amarillo">{visit}</p>
 									</aside>
 								</div>
 							</div>
@@ -282,38 +330,59 @@ const Tetris = () => {
 								{gameOver ? (
 									<Display text="Game Over" />
 								) : (
-										<div>
-											<Display text={`Score: ${score}`} />
-											<Display text={`rows: ${rows}`} />
-											<Display text={`Level: ${level}`} />
-											<Display text={`Speed: ${Math.round(dropTime)} ms`} />
-										</div>
-									)}
+									<div>
+										<Display text={`Score: ${score}`} />
+										<Display text={`rows: ${rows}`} />
+										<Display text={`Level: ${level}`} />
+										<Display
+											text={`Speed: ${Math.round(
+												dropTime
+											)} ms`}
+										/>
+									</div>
+								)}
 
-								<aside className='box_info' >
+								<aside className="box_info">
 									<form onSubmit={handleSubmit}>
 										{!currentPlayer ? (
-											<input className='user_input' onChange={handleChange} value={input} type='text' placeholder='Player Name' required />
+											<input
+												className="user_input"
+												onChange={handleChange}
+												value={input}
+												type="text"
+												placeholder="Player Name"
+												required
+											/>
 										) : (
-												<h2 className='user_name'>{currentPlayer}</h2>
-											)}
+											<h2 className="user_name">
+												{currentPlayer}
+											</h2>
+										)}
 										{input.length > 2 ? (
-
-											<StartButton type='submit' callback={startGame} />
+											<StartButton
+												type="submit"
+												callback={startGame}
+											/>
 										) : (
-												<StyledStartButton onClick={handleError} className='btn-sec'>Start Game</StyledStartButton>
-											)}
+											<StyledStartButton
+												onClick={handleError}
+												className="btn-sec"
+											>
+												Start Game
+											</StyledStartButton>
+										)}
 									</form>
 								</aside>
 							</aside>
 						</Zoom>
 					</StyledTetris>
-				</StyledTetrisWrapper >
+				</StyledTetrisWrapper>
 			) : (
-					<Loading loading={loading} />
-				)}
+				<Loading loading={loading} />
+			)}
 		</>
 	);
 };
 
 export default Tetris;
+
--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 export const StyledStage = styled.div`
 	display: grid;
 	grid-template-rows: repeat(
-		${props => props.height},
-		calc(25vw / ${props => props.width})
+		${(props) => props.height},
+		calc(22vw / ${(props) => props.width})
 	);
-	grid-template-columns: repeat(${props => props.width}, 1fr);
+	grid-template-columns: repeat(${(props) => props.width}, 1fr);
 	grid-gap: 1px;
-	border: 3px solid #2A9D8F;
+	border: 3px solid #2a9d8f;
 	width: 100%;
 	max-width: 25vw;
-	background-color: #2A9D8F;
+	max-height: 90%;
+	background-color: #2a9d8f;
 	border-radius: 5px;
 `;
